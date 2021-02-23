13877d72d8dacdfd369b3b9ea7d20d0947f3938b
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-02-23 14:12:53 +0100

    Add project files.

---
+++ MintPlayer.StringOps/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MintPlayer.StringOps.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>Makes the first character of a string upper-case.</summary>
+        /// <param name="str">Input string</param>
+        /// <returns>String with an upper-case first character.</returns>
+        public static string UcFirst(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+
+            char[] a = str.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
